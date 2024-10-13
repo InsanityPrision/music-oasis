b7a040044376460dba25b1f9e68ef7f8329df772
Aniol Rodriguez <arodrigue96@gmail.com>
2024-10-13 15:37:46 +0200

    Create render disc price function

--- src/types/ui/index.ts
+++ src/types/ui/index.ts
@@ -6,6 +6,21 @@ export const renderRecordCard = (
 ): void => {
   const element = recordCard.querySelector(".disc-card-title")!;
   element.textContent = `${record.name} (${record.artistName})`;
+
+  const price = recordCard.querySelector(".price")!;
+  price.textContent = `$${record.price}`;
+
+  const discountPrice = recordCard.querySelector(".discount");
+
+  if (record.price < record.originalPrice) {
+    if (discountPrice) {
+      discountPrice.textContent = `Discount: Original Price $${record.originalPrice}`;
+    }
+  } else {
+    if (discountPrice) {
+      discountPrice.remove();
+    }
+  }
 };
 
 export const renderRecordsList = (records: Disc[]): void => {
