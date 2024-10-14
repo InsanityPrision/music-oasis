a49521533c07833ec937fff08235dac5f74485e5
Aniol Rodriguez <arodrigue96@gmail.com>
2024-10-14 11:26:44 +0200

    Adjust HTML alts descriptions in HTML and index.ts

--- src/types/ui/index.ts
+++ src/types/ui/index.ts
@@ -8,8 +8,8 @@ export const renderRecordCard = (
   recordCover.src = record.imageUrl;
 
   const element = recordCard.querySelector(".album-and-artist-info")!;
+  recordCover.alt = `${record.name} album cover by ${record.artistName}`;
   element.textContent = `${record.name} (${record.artistName})`;
-  recordCover.alt = `${record.name} (${record.artistName})`;
 
   const price = recordCard.querySelector(".price")!;
   price.textContent = `$${record.price}`;
